b4d7f972ca35768142860035726233d1ce127ef1
Gabitoromero <garomero@frro.utn.edu.ar>
2026-01-12 10:39:25 -0300

    style: acttualizacion de logo

--- migrations/rename_liquidacionManual_to_liquidacion.sql
+++ migrations/rename_liquidacionManual_to_liquidacion.sql
@@ -8,16 +8,10 @@
 
 -- Renombrar columna liquidacionManual a liquidacion
 ALTER TABLE fleteros
-CHANGE COLUMN liquidacionManual liquidacion TINYINT(1) NOT NULL DEFAULT 0 COMMENT 'Indica si el fletero se liquida por CHESS (1) o automáticamente (0)';
+CHANGE COLUMN liquidacion_manual liquidacion TINYINT(1) NOT NULL DEFAULT 0 COMMENT 'Indica si el fletero se liquida por CHESS (1) o automáticamente (0)';
 
 -- Verificar el cambio
 DESCRIBE fleteros;
 
 -- Verificar datos
-SELECT
-    idFletero,
-    dsFletero,
-    seguimiento,
-    liquidacion
-FROM fleteros
-ORDER BY idFletero;
+SELECT * FROM fleteros ORDER BY id_fletero;
